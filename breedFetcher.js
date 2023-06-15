95f3639cab69523f1063afc675f6dd9b343d7fdd
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-15 14:30:27 -0700

    Refactor to modularize functions

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,18 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const breedFetcher = () => {
   request(url, (error, response, body) => {
     const data = JSON.parse(body);
 
     if (error) {
-      console.log('error:', error);
-      return;
-    } else if (data.length === 0) {
-      console.log(`${breedName} breed not found.`);
-      return;
-    } else {
-      console.log(data[0]);
+      callback(error, null);
+    }
+    if (data[0]) {
+      callback(null, data[0].description);
     }
   });
 };
 
-
-
-breedFetcher();
+module.exports = { fetchBreedDescription };
